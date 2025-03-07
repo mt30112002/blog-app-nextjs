25f0d4a38aaf461fa9d47f64cdd010dfcea9d896
minh trinh <mt30112002@gmail.com>
2025-03-07 20:04:57 +0700

    using swr get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 'use client';
-import { useEffect } from 'react';
+import React from "react";
+import useSWR from "swr";
 import Link from 'next/link'
 import AppTable from './components/app.table';
+
+
+
 const Home = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:8000/blogs');
-      const data = await response.json();
-      console.log(data);
+  
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
     }
-    fetchData();
-  }, [])
+  );
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+  console.log(data);
   return (
     <div>
+      <div>{data?.length}</div>
       <h1>Home</h1>
       <ul>
         <li><Link href={'/client/youtube'}>Youtube</Link></li>
